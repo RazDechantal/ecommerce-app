1ada4d559ce092226b3c480dced94380aebb7aa9
razdechantal <raz.dechantal@gmail.com>
2024-05-31 01:30:37 +0200

    some style changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/shop">Shop</Link></li>
-          <li><Link to="/cart">Cart</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
-    </header>
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top" className="custom-navbar">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Digital Market</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
+            <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
